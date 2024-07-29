501c2353ba56410abbb93ba4dbb16a48c5cbf002
Grant <2693470816@qq.com>
2024-07-29 21:10:16 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hello world
+// @namespace    https://grantg.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('hello world');
+})();
